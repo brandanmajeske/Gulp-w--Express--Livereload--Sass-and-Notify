ab8b8c88f27838f0cc05a22e4f85b64506d686e3
Brandan Majeske <bmajeske@me.com>
2014-03-01 13:57:58 -0700

    Added check for Winders

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var minifycss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
 var notify = require('gulp-notify');
 var sass = require('gulp-ruby-sass');
+var _if = require('gulp-if');
+var isWindows = /^win/.test(require('os').platform());
 var lr;
 var EXPRESS_PORT = 8080;
 var EXPRESS_ROOT = __dirname;
@@ -32,22 +34,33 @@ function notifyLiveReload(event) {
 	});	
 }
 
-
+/*
 gulp.task('css', function(){
 	return gulp.src('sass/main.scss')
 		.pipe(sass({style: 'compressed'}))
 		.pipe(autoprefixer('last 15 versions'))
 		.pipe(gulp.dest('css'))
 		.pipe(notify({ message: 'That\'s All Folks!'}));
-});
+});*/
+
+console.log("Is it Windows? " + isWindows);
+
+function reload(sass,autoprefixer,notify){
+	return gulp.src('sass/main.scss')
+		.pipe(sass({style: 'compressed'}))
+		.pipe(autoprefixer('last 15 versions'))
+		.pipe(gulp.dest('css'))
+		.pipe(_if(!isWindows, notify({ message: 'That\'s All Folks!'})));
+}
 
 gulp.task('default', function(){
 	startExpress();
 	startLiveReload();
+	reload(sass,autoprefixer,notify);
 	gulp.watch('*.html', notifyLiveReload);
 	gulp.watch('css/*.css', notifyLiveReload);
 
 	gulp.watch('sass/**/*.scss', function(){
-		gulp.run('css');
+		return reload(sass,autoprefixer,notify);
 	});
 });
